069641031ac09ede41e6982444db0ffd84685835
Romain <romainbillot>
2020-12-01 13:25:11 +0100

    example: accessToken and firecloudmessaging integration

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,31 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert';
+
 
 import 'package:flutter/services.dart';
 import 'package:twilio_programmable_voice/twilio_programmable_voice.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+
+  // Or do other work.
 }
 
 class MyApp extends StatefulWidget {
@@ -15,15 +35,65 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging();
+
+  Future<void> registerVoice() async {
+    // Generate accessToken from backend.
+    // http://localhost:3000/accessToken/test
+    final tokenResponse = await Dio().get("http://host:3000/accessToken/testId");
+
+    print("[TOKEN RESPONSE DATA]");
+    print(tokenResponse.data);
+    // Get fcmToken.
+    final fcmToken = await firebaseMessaging.getToken();
+    print("[FCM TOKEN]");
+    print(fcmToken);
+
+    TwilioProgrammableVoice.registerVoice(tokenResponse.data, fcmToken);
+  }
 
   @override
   void initState() {
     super.initState();
+
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        // It's a real push notification
+        if (message["notification"]["title"] != null) {
+
+        }
+
+        // It's a data
+        if (message.containsKey("data") && message["data"] != null) {
+          // It's a twilio data message
+          print("Message contains data");
+          if (message["data"].containsKey("twi_message_type")) {
+            print("Message is a Twilio Message");
+
+            final dataMap = Map<String, String>.from(message["data"]);
+
+            TwilioProgrammableVoice.handleMessage(dataMap);
+            print("handleMessage called in main.dart");
+          }
+        }
+      },
+      onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+
     initPlatformState();
 
     TwilioProgrammableVoice.requestMicrophonePermissions().then(print);
 
-    // TwilioProgrammableVoice.registerVoice(accessToken, fcmToken);
+    TwilioProgrammableVoice.addCallStatusListener(print);
+
+    this.registerVoice();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
