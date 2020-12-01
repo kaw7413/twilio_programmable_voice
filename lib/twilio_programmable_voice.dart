be0ca88570e208d7d5c2723cfff015aaf70d6b75
Romain <romainbillot>
2020-12-01 13:25:41 +0100

    feat(lib): call status listener and handle message facade

--- lib/twilio_programmable_voice.dart
+++ lib/twilio_programmable_voice.dart
@@ -4,8 +4,10 @@ import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class TwilioProgrammableVoice {
-  static const MethodChannel _channel =
-      const MethodChannel('twilio_programmable_voice');
+  static final MethodChannel _channel =
+      const MethodChannel('twilio_programmable_voice')..setMethodCallHandler(_handleMethod);
+
+  static List<void Function(Object)> onCallStatusCallbacks = <void Function(Object)>[];
 
   /// Request microphone permission on the platform
   ///
@@ -20,11 +22,38 @@ class TwilioProgrammableVoice {
     return microphonePermissionStatus;
   }
 
-  /// Delegate the registration to Twilio.
+  /// Delegate the registration to Twilio and start listening to call status.
   ///
   /// Throws an error if fail, the error returned by the Twilio Voice.register.
-  static Future<void> registerVoice(String accessToken, String fcmToken) async {
-    return _channel.invokeMethod('registerVoice');
+  static Future<void> registerVoice(String accessToken, String fcmToken) {
+    return _channel.invokeMethod('registerVoice', {"accessToken": accessToken, "fcmToken": fcmToken});
+  }
+
+  /// Add a listener to call status
+  static void addCallStatusListener(void Function(Object) callback) {
+    onCallStatusCallbacks.add(callback);
+  }
+
+  /// Add a previously registered listener
+  static void removeCallStatusListener(void Function(Object) callback) {
+    onCallStatusCallbacks.remove(callback);
+  }
+
+  /// Handle Fcm Message and delegate to Twilio
+  static Future<String> handleMessage(Map<String, String> data) {
+    return _channel.invokeMethod('handleMessage', {"messageData": data});
+  }
+
+  static Future<dynamic> _handleMethod(MethodCall call) async {
+    switch (call.method) {
+      case 'onCallStatusCallback':
+        for (final callStatusCallback in onCallStatusCallbacks) {
+          callStatusCallback(call.arguments);
+        }
+        break;
+      default:
+        throw ('method not defined');
+    }
   }
 
   // Platform specifics
