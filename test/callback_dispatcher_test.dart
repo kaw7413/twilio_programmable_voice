b5e7922056cc2182ba30f11c51975ba8957a4ca4
Romain <romainbillot>
2021-01-29 15:23:58 +0100

    test: callbackDispatcher

--- test/callback_dispatcher_test.dart
+++ test/callback_dispatcher_test.dart
@@ -1,9 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:twilio_programmable_voice/src/callback_dispatcher.dart';
+import 'package:twilio_programmable_voice/src/injector.dart';
+import 'package:workmanager/workmanager.dart';
+
+class WorkmanagerMock extends Mock implements Workmanager {}
 
 void main() {
-  test('callbackDispatcher should be a function', () {
+  setUpAll(() {
+    mockService<Workmanager>(mock: WorkmanagerMock());
+  });
+
+  test(
+      'callbackDispatcher should call Workmanager.executeTask with the taskHandler',
+      () {
     expect(callbackDispatcher, isA<Function>());
+    callbackDispatcher();
+    verify(getService<Workmanager>().executeTask(taskHandler));
   });
 }
