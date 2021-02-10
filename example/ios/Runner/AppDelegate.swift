0504fd255ada102f2c18f7197ffab1b12cf4058e
PIerre Monier <p.monier96@gmail.com>
2021-02-10 09:47:59 +0100

    feat: example app compile on real device with lots of exceptions

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,33 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+// TODO remove comment if the app start without it
+//      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+//      UNUserNotificationCenter.current().requestAuthorization(
+//        options: authOptions,
+//        completionHandler: {_, _ in })
+    }
+    
+//    GeneratedPluginRegistrant.register(with: self)
+    
+//    firebase_messaging set up
+
+//    else {
+//      let settings: UIUserNotificationSettings =
+//      UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+//      application.registerUserNotificationSettings(settings)
+//    }
+//
+//    application.registerForRemoteNotifications()
+    
+//    Messaging.messaging().delegate = self
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+    
+  override init() {
+    FirebaseApp.configure()
   }
 }
